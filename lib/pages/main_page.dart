b18bcde899ec6c91f6d060ef7f7760d3901d050a
deLkm <3063689268@qq.com>
2025-03-04 22:42:34 +0800

    Adjust

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -43,78 +43,77 @@ class _MainPageState extends State<MainPage> {
       shape: const CircularNotchedRectangle(),
       color: theme.primaryColor,
       notchMargin: 6.0,
-      height: 88,
+      height: 85,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.home, size: 28),
-            color: _selectedIndex == 0 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,
-            onPressed: () => _onItemTapped(0),
-          ),
-          Text(
-            'Home', 
-            style: TextStyle(color: _selectedIndex == 0 
-              ? theme.primaryColorDark 
-              : theme.scaffoldBackgroundColor
-            )
-          ),
-        ],
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.home, size: 23),
+                color: _selectedIndex == 0 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,
+                onPressed: () => _onItemTapped(0),
+              ),
+              Text(
+                'Home', 
+                style: TextStyle(color: _selectedIndex == 0 
+                  ? theme.primaryColorDark 
+                  : theme.scaffoldBackgroundColor
+                )
+              ),
+            ],
           ),
           Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.list, size: 28),
-            color: _selectedIndex == 1 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,
-            onPressed: () => _onItemTapped(1),
-          ),
-          Text(
-            'Plan', 
-            style: TextStyle(color: _selectedIndex == 1 
-              ? theme.primaryColorDark 
-              : theme.scaffoldBackgroundColor
-            )
-          ),
-        ],
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.list, size: 23),
+                color: _selectedIndex == 1 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,                  onPressed: () => _onItemTapped(1),
+              ),
+              Text(
+                'Plan', 
+                style: TextStyle(color: _selectedIndex == 1 
+                  ? theme.primaryColorDark 
+                  : theme.scaffoldBackgroundColor
+                )
+              ),
+            ],
           ),
           const SizedBox(width: 48), // 中间空出的位置给FloatingActionButton
           Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.pie_chart, size: 28),
-            color: _selectedIndex == 2 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,
-            onPressed: () => _onItemTapped(2),
-          ),
-          Text(
-            'Account', 
-            style: TextStyle(color: _selectedIndex == 2 
-              ? theme.primaryColorDark 
-              : theme.scaffoldBackgroundColor
-            )
-          ),
-        ],
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.pie_chart, size: 23),
+                color: _selectedIndex == 2 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,
+                onPressed: () => _onItemTapped(2),
+              ),
+              Text(
+                'Account', 
+                style: TextStyle(color: _selectedIndex == 2 
+                  ? theme.primaryColorDark 
+                  : theme.scaffoldBackgroundColor
+                )
+              ),
+            ],
           ),
           Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.person, size: 28),
-            color: _selectedIndex == 3 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,
-            onPressed: () => _onItemTapped(3),
-          ),
-          Text(
-            'Profile', 
-            style: TextStyle(color: _selectedIndex == 3 
-            ? theme.primaryColorDark 
-            : theme.scaffoldBackgroundColor
-            )
-          ),
-        ],
+          mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.person, size: 23),
+                color: _selectedIndex == 3 ? theme.primaryColorDark : theme.scaffoldBackgroundColor,
+                onPressed: () => _onItemTapped(3),
+              ),
+              Text(
+                'Profile', 
+                style: TextStyle(color: _selectedIndex == 3 
+                  ? theme.primaryColorDark 
+                  : theme.scaffoldBackgroundColor
+                )
+              ),
+            ],
           ),
         ],
       ),
